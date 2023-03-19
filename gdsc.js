66dbdd041ed3588bbe47df1026a07da706667a5c
Palash shah <113499117+shahpalash10@users.noreply.github.com>
2023-03-19 18:33:36 +0530

    Add files via upload

---
+++ gdsc.js
@@ -0,0 +1,80 @@
+window.addEventListener('load', () => {
+	const form = document.querySelector("#new-task-form");
+	const input = document.querySelector("#new-task-input");
+	const listval = document.querySelector("#tasks");
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		const task = input.value;
+
+		const taskp = document.createElement('div');
+		taskp.classList.add('task');
+
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
+
+		taskp.appendChild(task_content_el);
+
+		const task_input = document.createElement('input');
+		task_input.classList.add('text');
+		task_input.type = 'text';
+		task_input.value = task;
+		task_input.setAttribute('readonly', 'readonly');
+
+		task_content_el.appendChild(task_input);
+
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
+		
+		const task_edit = document.createElement('button');
+		task_edit.classList.add('edit');
+		task_edit.innerText = 'Edit';
+
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
+    
+    const task_updates=document.createElement('button');
+    task_updates.classList.add('update');
+    task_updates.innerText='Task Complete';
+    task_updates.style.color='#34A853';
+
+		task_actions_el.appendChild(task_edit);
+		task_actions_el.appendChild(task_delete_el);
+    task_actions_el.appendChild(task_updates);
+
+		taskp.appendChild(task_actions_el);
+
+		listval.appendChild(taskp);
+
+		input.value = '';
+
+		task_edit.addEventListener('click', (e) => {
+			if (task_edit.innerText.toLowerCase() == "edit") {
+				task_edit.innerText = "Save";
+				task_input.removeAttribute("readonly");
+				task_input.focus();
+			} else {
+				task_edit.innerText = "Edit";
+				task_input.setAttribute("readonly", "readonly");
+			}
+		});
+    task_updates.addEventListener('click', (e) =>{
+    	if (task_updates.innerText.toLowerCase() == "task complete"){
+      task_updates.innerText="Task Not Complete";
+      task_updates.style.color="#DC143C";
+      }
+      else
+      {
+      task_updates.innerText="Task Complete";
+      task_updates.style.color="#34A853";
+      }
+
+    });
+
+		task_delete_el.addEventListener('click', (e) => {
+			listval.removeChild(taskp);
+		});
+	});
+});
